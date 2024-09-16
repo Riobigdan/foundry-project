5a5ab89733da2329bcafde512563b9387e3e6387
rioBigDan <riozron@outlook.com>
2024-09-16 12:53:47 +0800

    cast wallet way to save priavate key

--- tt01-simple-storage/script/DeploySimpleStorage.s.sol
+++ tt01-simple-storage/script/DeploySimpleStorage.s.sol
@@ -6,9 +6,8 @@ import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
-        uint256 deployerPrivateKey = vm.envOr("PRIVATE_KEY", uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
         
-        vm.startBroadcast(deployerPrivateKey);
+        vm.startBroadcast();
         SimpleStorage simpleStorage = new SimpleStorage();
         vm.stopBroadcast();
         
