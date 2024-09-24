9ba1af99bb8d89d3409295e312b7d91819a62e0c
RioBigDan <riozron@outlook.com>
2024-09-24 23:39:45 +0800

    fix the linktoken

--- tt04-lottery/script/HelperConfig.s.sol
+++ tt04-lottery/script/HelperConfig.s.sol
@@ -38,6 +38,10 @@ contract HelperConfig is Script, CodeChainCode {
         networkConfig[SEPOLIA_CHAIN_ID] = getSepoliaConfig();
     }
 
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
     /**
      * @dev 根据chainId获取网络配置
      * @param chainId 链id
@@ -47,14 +51,10 @@ contract HelperConfig is Script, CodeChainCode {
         if (networkConfig[chainId].vrfCoordinator != address(0)) {
             return networkConfig[chainId];
         } else if (chainId == ANVIL_CHAIN_ID) {
-            return getOrCreateAnvilConfig();
+            return getAnvilConfig();
         } else {
             revert HelperConfig__NetworkNotConfigured(chainId);
         }
-    }
-
-    function getConfig() public returns (NetworkConfig memory) {
-        return getConfigByChainId(block.chainid);
     }
 
     function getSepoliaConfig() public pure returns (NetworkConfig memory) {
@@ -64,31 +64,31 @@ contract HelperConfig is Script, CodeChainCode {
             vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             subscriptionId: 84197343675464504425414728616412005354770664351257355224211786028223568062920,
-            callbackGasLimit: 500000,
-            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+            link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+            callbackGasLimit: 500000
         });
     }
 
     /**
-     * @dev 获取本地的anvil配置
-     * 因为要部署一些合约 所以不是pure
+     * @dev 获取anvil配置
+     * @return NetworkConfig 网络配置
+     * @notice
+     * 配置 Mock VRFCoordinator (VRFCoordinator 是 Chainlink 的合约 用于管理 VRF 的请求和响应)
+     * 配置 Mock LinkToken (LinkToken 是 Chainlink 的代币 用于支付 VRF 费用)
      */
-    function getOrCreateAnvilConfig() public returns (NetworkConfig memory) {
+    function getAnvilConfig() public returns (NetworkConfig memory) {
         // @dev 如果已经配置了 直接返回
         if (localNetworkConfig.vrfCoordinator != address(0)) {
             return localNetworkConfig;
         }
 
-        // @dev 如果没有配置 则创建MockVrfCoordinator
         vm.startBroadcast();
         VRFCoordinatorV2_5Mock vrfCoordinator = new VRFCoordinatorV2_5Mock(
             ANVIL_BASE_FEE, // baseFee
             ANVIL_GAS_PRICE, // gasPrice
             ANVIL_WEI_PER_UNIT_LINK // weiPerUnitLink
         );
-        // @dev Deploy Link Token
         LinkToken linkToken = new LinkToken();
-
         vm.stopBroadcast();
 
         localNetworkConfig = NetworkConfig({
--- tt04-lottery/test/mocks/LinkToken.sol
+++ tt04-lottery/test/mocks/LinkToken.sol
@@ -3,33 +3,73 @@ pragma solidity ^0.8.18;
 
 import {ERC20} from "@solmate/tokens/ERC20.sol";
 
-/**
- * @title LinkToken
- * @author
- * @notice 创建一个ERC677Receiver接口，用于接收ERC677代币的转移
- */
 interface ERC677Receiver {
-    function onTokenTransfer(address from, uint256 amount, bytes calldata data) external returns (bool);
+    /**
+     * @notice 接收代币的回调函数
+     * @param _from 发送代币的地址
+     * @param _value 转移的代币数量
+     * @param _data 回调函数的数据
+     */
+    function onTokenTransfer(address _from, uint256 _value, bytes calldata _data) external;
 }
 
 /**
  * @title LinkToken
  * @author
- * @notice 创建一个ERC677Receiver接口，用于接收ERC677代币的转移
+ * @notice LinkToken 继承自 ERC20，这是一个标准的代币实现，提供了基本的代币功能如余额追踪和转账。
  */
 contract LinkToken is ERC20 {
     uint256 constant INITIAL_SUPPLY = 1000000000000000000;
     uint8 constant DECIMALS = 18;
 
+    /**
+     * @notice 构造函数，初始化代币供应量
+     */
     constructor() ERC20("Chainlink Token", "LINK", DECIMALS) {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
+    /**
+     * @notice 铸造代币
+     * @param to 接收代币的地址
+     * @param amount 铸造的代币数量
+     */
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 
+    /**
+     * @notice 转移代币并调用回调函数
+     * @param to 接收代币的地址
+     * @param amount 转移的代币数量
+     * @param data 回调函数的数据
+     * @return 是否成功转移代币
+     */
     function transferAndCall(address to, uint256 amount, bytes calldata data) public returns (bool) {
-        return ERC677Receiver(to).onTokenTransfer(msg.sender, amount, data);
+        ERC677Receiver(to).onTokenTransfer(msg.sender, amount, data);
+        return true;
+    }
+
+    /**
+     * @notice private 合约回调函数
+     * @param _to 接收代币的地址
+     * @param _value 转移的代币数量
+     * @param _data 回调函数的数据
+     */
+    function contractFallback(address _to, uint256 _value, bytes memory _data) private {
+        ERC677Receiver(_to).onTokenTransfer(msg.sender, _value, _data);
+    }
+
+    /**
+     * @notice 判断地址是否为合约
+     * @param account 要判断的地址
+     * @return 是否为合约
+     */
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
     }
 }
